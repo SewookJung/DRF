7027974999fe5fdf5d562bc3815c9ec0ef8b6c9f
sewookJung <tpdnrqkqh@naver.com>
2021-08-21 21:14:10 +0900

    [ADD] CRUD by function base api views

--- api_basic/views.py
+++ api_basic/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework import status
+from rest_framework.parsers import JSONParser
 
 from .models import Article
 from .serializers import ArticleSerializer
@@ -20,6 +22,39 @@ def article_list(request):
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+
         else:
-            return JsonResponse(serializer.error, status=400)
+            return JsonResponse(
+                serializer.error_messages, status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+@csrf_exempt
+def article_detail(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+
+    except Article.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
+
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(article, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_204_NO_CONTENT)
+
+        else:
+            return JsonResponse(
+                serializer.error_messages, status=status.HTTP_400_BAD_REQUEST
+            )
+
+    elif request.method == "DELETE":
+        article.delete()
+        return HttpResponse(status.HTTP_204_NO_CONTENT)
