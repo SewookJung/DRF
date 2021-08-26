b9e67787489d4f6144f3479f0509416a223d9f21
sewookJung <tpdnrqkqh@naver.com>
2021-08-26 15:15:58 +0900

    [ADD] Testing authentication by DRF token

--- api_basic/views.py
+++ api_basic/views.py
@@ -3,6 +3,12 @@ from rest_framework import status, generics, mixins
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework.authentication import (
+    SessionAuthentication,
+    BasicAuthentication,
+    TokenAuthentication,
+)
+from rest_framework.permissions import IsAuthenticated
 
 
 from .models import Article
@@ -21,6 +27,9 @@ class GenericApiView(
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     lookup_field = "pk"
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, pk=None):
         if pk:
